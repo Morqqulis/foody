853f1991800194ae8d9bd716e5c1da1c955a7602
Morqulis <morqqulis@gmail.com>
2024-07-18 08:46:44 +0400

    added translation for User page metadata

--- src/app/[locale]/(client)/user/page.tsx
+++ src/app/[locale]/(client)/user/page.tsx
@@ -1,4 +1,14 @@
-import { NextPage } from "next";
+import { NextPage } from "next"
+import { getTranslations } from 'next-intl/server'
+
+export async function generateMetadata({ params: { locale } }) {
+    const t = await getTranslations({ locale, namespace: "User.Metadata" });
+  
+    return {
+      title: t("title"),
+      description: t("description"),
+    };
+  }
 
 interface IUserPage {}
 
