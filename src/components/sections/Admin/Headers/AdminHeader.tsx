82dfab7a770066fb3f92b2f9ec4086c5e6651d22
Morqulis <morqqulis@gmail.com>
2024-07-18 20:52:11 +0400

    Added translation for AdminHeader component

--- src/components/sections/Admin/Headers/AdminHeader.tsx
+++ src/components/sections/Admin/Headers/AdminHeader.tsx
@@ -1,11 +1,13 @@
 import HeaderLanguages from "@sections/Header/HeaderLangs";
 import { Button } from "@ui/button";
 import Logo from "@ui/Logo";
+import { useTranslations } from 'next-intl'
 import Image from "next/image";
 
 interface IAdminHeader {}
 
 const AdminHeader: React.FC = (): JSX.Element => {
+    const t = useTranslations('Admin.Header')
   return (
     <header className={`mb-[18px] rounded-b-[20px] bg-[#27283C] p-3`}>
       <nav className={`flex items-center justify-between gap-5`}>
@@ -16,12 +18,12 @@ const AdminHeader: React.FC = (): JSX.Element => {
             type={"button"}
           >
             <span className={`text-lg`}>+</span>
-            Add Product
+            {t('btn')}
           </Button>
           <HeaderLanguages />
           <div className={`flex items-center gap-2 text-white`}>
             <Image className={``} src="/Admin/Header/admin.svg" alt={"admin icon"} width={40} height={40} />
-            <span className={`translate-y-[10px]`}>Admin</span>
+            <span className={`translate-y-[10px]`}>{t('admin')}</span>
           </div>
         </div>
       </nav>
