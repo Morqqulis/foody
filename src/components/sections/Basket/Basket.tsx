3fc82914a5abab26f1d45d922b312a4b6aab9276
Fidan-Alizada <160149958+Fidan-Alizada@users.noreply.github.com>
2024-07-18 15:27:55 +0400

    g

--- src/components/sections/Basket/Basket.tsx
+++ src/components/sections/Basket/Basket.tsx
@@ -54,11 +54,9 @@ const Basket = () => {
   }, [basketItems]);
 
   const handleQuantityChange = (id, delta) => {
-    setBasketItems((prevItems) =>
-      prevItems.map((item) =>
-        item.id === id ? { ...item, quantity: Math.max(1, item.quantity + delta) } : item
-      )
-    );
+    setBasketItems((prevItems) => {
+      return prevItems.map((item) => (item.id === id ? { ...item, quantity: Math.max(1, item.quantity + delta) } : item));
+    });
   };
 
   const handleDelete = (id) => {
@@ -70,22 +68,22 @@ const Basket = () => {
   return (
     <section>
       <div className="container">
-        <div className="mt-4 flex justify-end">
-          <div className="w-3/4 bg-gray-700 p-10 rounded-lg shadow-md">
-            <div className="mb-6">
+        <div className="mt-4 flex  justify-end  ">
+          <div className="w-3/4 bg-gray-7  p-10">
+            <div className="mb-6 flex flex-col">
               <div className="flex items-center">
-                <ShoppingCart className="h-5 w-5 text-red-600" />
-                <h1 className="ml-2 text-2xl font-semibold text-gray-900">{t("title")}</h1>
+                <h1 className="text-2xl font-semibold text-gray-950">{t("title")}</h1>
               </div>
               <div className="mt-2 flex items-center">
-                <h5 className="text-red-600">
+                <ShoppingCart className="h-5 w-5 text-red-600" />
+                <h5 className="ml-2 text-red-600">
                   {totalItems} {t("items")}
                 </h5>
               </div>
             </div>
             <div className={styles.customScrollbar}>
               {basketItems.map((item) => (
-                <div key={item.id} className="mb-4 flex justify-between items-center p-4 bg-white rounded-lg shadow-md">
+                <div key={item.id} className="mb-4 flex items-center justify-between rounded-lg bg-white p-4 shadow-md">
                   <div className="flex items-center">
                     <Image className="h-20 w-20" src={item.img_url} alt={item.name} width={80} height={80} />
                     <div className="ml-4">
@@ -94,24 +92,24 @@ const Basket = () => {
                     </div>
                   </div>
                   <div className="flex items-center">
-                    <div className="flex items-center">
+                    <div className="flex flex-col items-center">
                       <button
                         onClick={() => handleQuantityChange(item.id, 1)}
-                        className="h-8 w-8 flex items-center justify-center rounded-full bg-gray-200"
+                        className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-200"
                       >
                         <Plus className="h-5 w-5" />
                       </button>
                       <span className="px-4">{item.quantity}</span>
                       <button
                         onClick={() => handleQuantityChange(item.id, -1)}
-                        className="h-8 w-8 flex items-center justify-center rounded-full bg-gray-200"
+                        className="flex h-8 w-8 items-center justify-center rounded-full bg-gray-200"
                       >
                         <Minus className="h-5 w-5" />
                       </button>
                     </div>
                     <button
                       onClick={() => handleDelete(item.id)}
-                      className="ml-2 h-10 w-10 flex items-center justify-center rounded-full bg-gray-200"
+                      className="ml-2 flex h-10 w-10 items-center justify-center rounded-full bg-gray-200"
                     >
                       <Trash2 className="h-5 w-5 text-red-600 hover:text-red-700" />
                     </button>
@@ -119,7 +117,7 @@ const Basket = () => {
                 </div>
               ))}
             </div>
-            <div className="mt-4 flex items-center justify-between bg-red-600 rounded-full p-4 shadow-md">
+            <div className="mt-4 flex items-center justify-between rounded-full bg-red-600 p-4 shadow-md">
               <button className="rounded-lg bg-red-600 px-4 py-2 text-white">{t("checkout")}</button>
               <span className="rounded-3xl bg-white px-4 py-2 text-xl font-semibold text-red-600">$ {totalAmount.toFixed(2)}</span>
             </div>
