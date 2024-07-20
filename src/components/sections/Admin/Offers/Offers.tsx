c212ad3bdc27368bf97c6dc2727ad586d2cb416c
Fidan-Alizada <160149958+Fidan-Alizada@users.noreply.github.com>
2024-07-21 00:25:46 +0400

    last changes

--- src/components/sections/Admin/Offers/Offers.tsx
+++ src/components/sections/Admin/Offers/Offers.tsx
@@ -1,102 +1,123 @@
-import AdminAside from "@sections/Admin/Aside/AdminAside";
+"use client";
 import { NextPage } from "next";
+import { useState } from "react";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
+import AddOfferModal from "@sections/Admin/Offers/AddOfferModal";
+import EditOfferModal from "@sections/Admin/Offers/EditOfferModal";
+import SectionHeader from "@sections/Admin/Headers/SectionHeader";
+import { Edit2, Trash } from "lucide-react";
 
-const offers = [
-  {
-    id: 9177,
-    image: "/images/pizza.png", // Replace with the actual image path
-    title: "Do you like Pizza at Papa's?",
-    description: "Yummy thin pizza with pepperoni",
-  },
-  {
-    id: 9187,
-    image: "/images/pizza.png", // Replace with the actual image path
-    title: "Do you like Pizza at Papa's?",
-    description: "Yummy thin pizza with pepperoni",
-  },
-  {
-    id: 9179,
-    image: "/images/pizza.png", // Replace with the actual image path
-    title: "Do you like Pizza at Papa's?",
-    description: "Yummy thin pizza with pepperoni",
-  },
-  // Add more offers as needed
-];
+interface Offer {
+  id: number;
+  image: string;
+  title: string;
+  description: string;
+}
 
 const OffersPage: NextPage = (): JSX.Element => {
+  const t = useTranslations("OffersPage");
+
+  const [offers, setOffers] = useState<Offer[]>([
+    {
+      id: 9177,
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdwXtAOL0wCTsYOlr33hJX33swJ5ItWUmeRA&s",
+      title: "Do you like Pizza at Papa's?",
+      description: "Yummy thin pizza with chicken",
+    },
+    {
+      id: 9178,
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdwXtAOL0wCTsYOlr33hJX33swJ5ItWUmeRA&s",
+      title: "Do you like Pizza at Papa's?",
+      description: "Yummy thin pizza with pepperoni",
+    },
+    {
+      id: 9179,
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdwXtAOL0wCTsYOlr33hJX33swJ5ItWUmeRA&sg",
+      title: "Do you like Pizza at Papa's?",
+      description: "Yummy thin pizza with meat",
+    },
+  ]);
+
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [currentOffer, setCurrentOffer] = useState<Offer | null>(null);
+
+  const openAddModal = () => setIsAddModalOpen(true);
+  const closeAddModal = () => setIsAddModalOpen(false);
+
+  const openEditModal = (offer: Offer) => {
+    setCurrentOffer(offer);
+    setIsEditModalOpen(true);
+  };
+  const closeEditModal = () => setIsEditModalOpen(false);
+
+  const addOffer = (offer: Offer) => {
+    setOffers([...offers, offer]);
+  };
+
+  const editOffer = (updatedOffer: Offer) => {
+    setOffers(offers.map((offer) => (offer.id === updatedOffer.id ? updatedOffer : offer)));
+  };
+
+  const deleteOffer = (offerId: number) => {
+    setOffers(offers.filter((offer) => offer.id !== offerId));
+  };
+
   return (
-    <div className="flex min-h-screen bg-gray-900 text-white">
-      <AdminAside />
-      <main className="flex-grow p-6">
-        <div className="flex justify-between items-center mb-4">
-          <h1 className="text-2xl font-bold">Offers</h1>
-          <button className="bg-pink-500 text-white px-4 py-2 rounded-lg hover:bg-pink-600">
-            + ADD OFFER
+    <div className="flex min-h-screen text-white">
+      <div className="flex-1 p-6">
+        <SectionHeader title={t('title')}/>
+          <button
+            onClick={openAddModal}
+            className="bg-pink-500 text-white px-4 py-2 rounded hover:bg-pink-600"
+          >
+            {t('addOffer')}
           </button>
-        </div>
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-gray-800 rounded-lg">
+       
+        <div className="overflow-y-auto bg-gray-100">
+          <table className="min-w-full text-gray-800">
             <thead>
               <tr>
-                <th className="px-4 py-2 border border-gray-700">ID</th>
-                <th className="px-4 py-2 border border-gray-700">Image</th>
-                <th className="px-4 py-2 border border-gray-700">Title</th>
-                <th className="px-4 py-2 border border-gray-700">Description</th>
-                <th className="px-4 py-2 border border-gray-700">Actions</th>
+                <th className="px-4 py-2 border border-gray-200">{t('id')}</th>
+                <th className="px-4 py-2 border border-gray-200">{t('image')}</th>
+                <th className="px-4 py-2 border border-gray-200">{t('titleColumn')}</th>
+                <th className="px-4 py-2 border border-gray-200">{t('description')}</th>
+                <th className="px-4 py-2 border border-gray-200">{t('actions')}</th>
               </tr>
             </thead>
             <tbody>
-              {offers.map((offer, index) => (
-                <tr key={index}>
-                  <td className="border border-gray-700 px-4 py-2">{offer.id}</td>
-                  <td className="border border-gray-700 px-4 py-2">
+              {offers.map((offer) => (
+                <tr key={offer.id}>
+                  <td className="px-4 py-2 border border-gray-200">{offer.id}</td>
+                  <td className="px-4 py-2 border border-gray-200">
                     <Image src={offer.image} width={50} height={50} alt={offer.title} />
                   </td>
-                  <td className="border border-gray-700 px-4 py-2">{offer.title}</td>
-                  <td className="border border-gray-700 px-4 py-2">{offer.description}</td>
-                  <td className="border border-gray-700 px-4 py-2">
-                    <div className="flex space-x-2">
-                      <button className="text-green-500 hover:text-green-700">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          className="w-5 h-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M11 17l2-2m0 0l-2-2m2 2h8m-8 0H5m8 0V3m0 18V3"
-                          />
-                        </svg>
-                      </button>
-                      <button className="text-red-500 hover:text-red-700">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          className="w-5 h-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M6 18L18 6M6 6l12 12"
-                          />
-                        </svg>
-                      </button>
-                    </div>
+                  <td className="px-4 py-2 border border-gray-200">{offer.title}</td>
+                  <td className="px-4 py-2 border border-gray-200">{offer.description}</td>
+                  <td className="px-4 py-6 border flex justify-center items-center gap-5">
+                    <button onClick={() => openEditModal(offer)}>
+                      <Edit2 className="w-5 h-5 text-green-500 hover:text-gray-400" />
+                    </button>
+                    <button onClick={() => deleteOffer(offer.id)}>
+                      <Trash className="w-5 h-5 text-red-500 hover:text-gray-400" />
+                    </button>
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
-      </main>
+      </div>
+      <AddOfferModal isOpen={isAddModalOpen} onClose={closeAddModal} onSave={addOffer} />
+      {isEditModalOpen && currentOffer && (
+        <EditOfferModal
+          isOpen={isEditModalOpen}
+          onClose={closeEditModal}
+          offer={currentOffer}
+          onSave={editOffer}
+        />
+      )}
     </div>
   );
 };
