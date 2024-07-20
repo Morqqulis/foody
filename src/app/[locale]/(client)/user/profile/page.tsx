ce6a352327563d9134fb195444a8b9fc2d792c68
Elvin <elvinnk@code.edu.az>
2024-07-21 03:22:07 +0400

    Added svg folder, svg photo in profile and placeholder

--- src/app/[locale]/(client)/user/profile/page.tsx
+++ src/app/[locale]/(client)/user/profile/page.tsx
@@ -1,4 +1,5 @@
-import { GetServerSideProps, NextPage } from "next";
+import { NextPage } from 'next';
+import UploadImageSvg from '../../../../../../public/userProfileClient/fileUploadProfile.svg';
 
 interface IUserProfilePage {
   contact: string;
@@ -10,36 +11,37 @@ interface IUserProfilePage {
 
 const UserProfilePage: NextPage<IUserProfilePage> = ({ contact, username, fullName, email, address }) => {
   return (
-    <main className="p-6">
+    <main className="p-6 bg-gray-100">
       <h1 className="mb-4 text-2xl font-bold">Profile</h1>
       <div className="mb-6 flex flex-col items-center">
-        <div className="mb-4 flex h-24 w-24 items-center justify-center rounded-full bg-gray-200">
-          <button className="text-green-500">upload</button>
+        <div className="mb-4 flex flex-col items-center justify-center rounded-full bg-white p-4">
+          <UploadImageSvg className="h-14 w-16" />
+          <button className="text-gray-500">upload</button>
         </div>
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         <div className="flex flex-col">
-          <label className="mb-2">Contact</label>
-          <input type="text" defaultValue={contact} className="rounded border p-2" readOnly />
+          <label className="mb-2 font-bold">Contact</label>
+          <input type="text" defaultValue={contact} className="rounded border p-2" readOnly placeholder="+994 77 555 55 55" />
         </div>
         <div className="flex flex-col">
-          <label className="mb-2">Email</label>
-          <input type="email" defaultValue={email} className="rounded border p-2" readOnly />
+          <label className="mb-2 font-bold">Email</label>
+          <input type="email" defaultValue={email} className="rounded border p-2" readOnly placeholder="kamilovelvin4@gmail.com"  />
         </div>
         <div className="flex flex-col">
-          <label className="mb-2">Username</label>
-          <input type="text" defaultValue={username} className="rounded border p-2" readOnly />
+          <label className="mb-2 font-bold">Username</label>
+          <input type="text" defaultValue={username} className="rounded border p-2" readOnly placeholder="elvinkamilov"  />
         </div>
         <div className="flex flex-col">
-          <label className="mb-2">Address</label>
-          <input type="text" defaultValue={address} className="rounded border p-2" readOnly />
+          <label className="mb-2 font-bold">Address</label>
+          <input type="text" defaultValue={address} className="rounded border p-2" readOnly placeholder="Khocali 29 Khatai Baku"  />
         </div>
         <div className="flex flex-col">
-          <label className="mb-2">Full Name</label>
-          <input type="text" defaultValue={fullName} className="rounded border p-2" readOnly />
+          <label className="mb-2 font-bold">Full Name</label>
+          <input type="text" defaultValue={fullName} className="rounded border p-2" readOnly placeholder="Kamilov Elvin" />
         </div>
         <div className="flex flex-col">
-        <button className="mt-8 rounded bg-green-500 p-2 text-white">Save</button>
+          <button className="mt-8 rounded bg-green-500 p-2 text-white">Save</button>
         </div>
       </div>
     </main>
