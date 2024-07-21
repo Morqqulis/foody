21367e892f849edea6f88a42c9e8daddacaf9c1a
Fidan-Alizada <160149958+Fidan-Alizada@users.noreply.github.com>
2024-07-21 22:28:14 +0400

    header

--- src/components/sections/Restaurants/Header.tsx
+++ src/components/sections/Restaurants/Header.tsx
@@ -1,23 +1,36 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-between p-4 bg-white shadow">
-      <div>
-        <h1 className="text-xl font-bold">Papa John’s Pizza Restaurant</h1>
-        <p className="text-gray-500">19 Nizami street, Sabail, Baku</p>
+    <div>
+      <div className="flex justify-center p-4">
+        <Image 
+          src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Papa_John%27s_Logo_2019.svg" 
+          alt="Restaurant" 
+          width={1372} 
+          height={537} 
+        />
       </div>
-      <div className="flex items-center space-x-4">
-        <div>
-          <p className="text-gray-500 text-right">Cuisine</p>
-          <p className="text-gray-500">pizza, drink, hotdog, sendvich, roll</p>
+      <div className="flex items-center justify-between p-4 bg-white shadow">
+        <div className="flex items-center">
+          <div className="ml-4">
+            <h1 className="text-xl font-bold">Papa John’s Pizza Restaurant</h1>
+            <p className="text-gray-500">19 Nizami street, Sabail, Baku</p>
+          </div>
         </div>
-        <button className="px-4 py-2 border border-red-500 text-red-500 rounded">
-          $5 Delivery
-        </button>
-        <button className="px-4 py-2 bg-red-500 text-white rounded">
-          Go Back
-        </button>
+        <div className="flex items-center space-x-4">
+          <div>
+            <p className="text-gray-500 text-right">Cuisine</p>
+            <p className="text-gray-500">pizza, drink, hotdog, sendvich, roll</p>
+          </div>
+          <button className="px-4 py-2 border border-red-500 text-red-500 rounded">
+            $5 Delivery
+          </button>
+          <button className="px-4 py-2 bg-red-500 text-white rounded">
+            Go Back
+          </button>
+        </div>
       </div>
     </div>
   )
