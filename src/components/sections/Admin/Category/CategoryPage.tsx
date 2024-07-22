1e2d55a43cb64c382fe3d3e02d988ea91f7179af
aylaya <160137722+aylaya@users.noreply.github.com>
2024-07-22 10:55:06 +0400

    translation

--- src/components/sections/Admin/Category/CategoryPage.tsx
+++ src/components/sections/Admin/Category/CategoryPage.tsx
@@ -1,9 +1,10 @@
-"use client";
+'use client';
 
 import { FC, useState } from 'react';
 import { PlusCircle, Edit, Trash } from 'lucide-react';
-import Image from 'next/image'; // Import Image from next/image
+import Image from 'next/image'; 
 import CategoryModal from './CategoryModal';
+import { useTranslations } from 'next-intl';
 
 type Category = {
   id: number;
@@ -19,10 +20,10 @@ const initialCategories: Category[] = [
   { id: 9177, image: 'https://placehold.jp/150x150.png', name: 'Pizza', slug: 'yummy-pizza' },
   { id: 9178, image: 'https://placehold.jp/150x150.png', name: 'Sandvic', slug: 'sendvic' },
   { id: 9179, image: 'https://placehold.jp/150x150.png', name: 'Fries', slug: 'fries' },
-  
 ];
 
 const AdminRightSidebar: FC = () => {
+  const t = useTranslations('Category');
   const [categories, setCategories] = useState<Category[]>(initialCategories);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentCategory, setCurrentCategory] = useState<Category | undefined>(undefined);
@@ -46,22 +47,22 @@ const AdminRightSidebar: FC = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen w-2/3  p-6">
+    <div className="flex flex-col h-screen w-full p-6">
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-xl font-bold">Category</h2>
+        <h2 className="text-xl font-bold">{t('title')}</h2>
         <button onClick={() => openModal()} className="bg-purple-700 text-white p-2 rounded-md">
-          <PlusCircle className="inline mr-2" /> Add Category
+          <PlusCircle className="inline mr-2" /> {t('addCategory')}
         </button>
       </div>
       <div className="overflow-y-auto bg-gray-100">
         <table className="min-w-full bg-white">
           <thead>
             <tr>
-              <th className="py-2 px-4 border-b">ID</th>
-              <th className="py-2 px-4 border-b">Image</th>
-              <th className="py-2 px-4 border-b">Name</th>
-              <th className="py-2 px-4 border-b">Slug</th>
-              <th className="py-2 px-4 border-b">Actions</th>
+              <th className="py-2 px-4 border-b">{t('id')}</th>
+              <th className="py-2 px-4 border-b">{t('image')}</th>
+              <th className="py-2 px-4 border-b">{t('name')}</th>
+              <th className="py-2 px-4 border-b">{t('slug')}</th>
+              <th className="py-2 px-4 border-b">{t('actions')}</th>
             </tr>
           </thead>
           <tbody>
@@ -69,12 +70,12 @@ const AdminRightSidebar: FC = () => {
               <tr key={category.id}>
                 <td className="py-2 px-4 border-b">{category.id}</td>
                 <td className="py-2 px-4 border-b">
-                  <Image src={category.image} alt={category.name} width={32} height={32} /> {/* Use Image component */}
+                  <Image src={category.image} alt={category.name} width={32} height={32} /> 
                 </td>
                 <td className="py-2 px-4 border-b">{category.name}</td>
                 <td className="py-2 px-4 border-b">{category.slug}</td>
-                <td className="py-2 px-4 border-b">
-                  <button onClick={() => openModal(category)} className="text-blue-500 hover:text-blue-700 mx-1">
+                <td className="py-2 px-4 border-b ">
+                  <button onClick={() => openModal(category)} className="text-blue-500 hover:text-blue-700 mx-1 ">
                     <Edit size={16} />
                   </button>
                   <button onClick={() => handleDeleteCategory(category.id)} className="text-red-500 hover:text-red-700 mx-1">
