58a3a74d039de0e9bb0d6514509c41480133d878
PakizaMutallimova <pakizemutellimova97@mail.ru>
2024-07-23 20:42:59 +0400

    fixed card

--- src/components/sections/Restaurants/RestaurantCard.tsx
+++ src/components/sections/Restaurants/RestaurantCard.tsx
@@ -9,43 +9,30 @@ interface IRestCard {
         description: string;
         deliveryPrice: number;
         deliveryMin: number;
+        new_item?: boolean;
     }
 }
 
 const RestaurantCard: FC<IRestCard> = ({ data }): JSX.Element => {
-    const { image, title, deliveryMin, deliveryPrice, description } = data
+    const { image, title, deliveryMin, deliveryPrice, description, new_item } = data
     return (
-        // <div className='w-[235px] bg-slate-100'>
-        //     <Image className=' overflow-hidden bg-cover bg-center' src={image} alt={title} width={240} height={130} />
-        //     <div>
-        //         <p>{title}</p>
-        //         <p>{description}</p>
-        //         <div>
-        //             <div>
-        //                 <span>${deliveryPrice}</span>
-        //                 <p>Delivery</p>
-        //             </div>
-        //             <div>{deliveryMin} Min</div>
-        //         </div>
-        //     </div>
-        // </div>
-        <div className="flex bg-zinc-600 h-[345px] w-[200px] items-center justify-center rounded-[5px] bg-white shadow-[0px_4px_4px_0px_rgb(57_57_57)/25%]">
-            <div className="h-[245px] w-[163px]">
-                <Image src={`https://picsum.photos/id//160/158`} alt="image" width={160} height={158} />
-
+        <div className="flex  h-[345px] w-[200px] relative justify-center rounded-[5px] bg-white" style={{ boxShadow: '0px 0px 5px 3px #00000040' }}>
+            {new_item && 
+                <div className='w-[53px] h-[27px] bg-[#D63626] absolute top-0 left-0 text-white flex items-center justify-center text-[12px]'>New</div>
+            }
+            <div className="h-full w-[163px] flex justify-between flex-col pt-4 pb-4">
+                <Image src={image} alt="image" width={160} height={158} />
                 <div className="mt-2  w-[100%] whitespace-nowrap ">
-                    <h1 className=" h-[32px] w-[100%]   text-lg font-medium leading-[24px] text-[#1E1E30]">{data.title}</h1>
-                    <p className=" h-[32px] w-[100%] text-sm  font-medium leading-[24px]  text-[#8E8E93]">Rest name</p>
-                </div>
 
+                    <p className=" h-[32px] w-full   text-lg font-medium leading-[24px] text-[#1E1E30]">{title}</p>
+                    <p className=" h-[32px] w-full text-sm overflow-hidden  font-medium leading-[24px]  text-[#8E8E93]">{description.substring(0, 40) }</p>
+                </div>
                 <div className="flex items-center justify-between">
-                    <div className='flex justify-start'>
-                        <p className=" h-[21px] w-[50.77px]  text-[12px] font-medium leading-[24px] text-[#00B2A9]">${data.deliveryPrice}</p>
+                    <div className='flex justify-items-start w-2/4 gap-2'>
+                        <p className=" h-[21px] w-[50.77px]  text-[12px] font-medium leading-[24px]">${deliveryPrice}</p>
                         <p>Delivery</p>
                     </div>
-                    <div className=" bg-[#D63626] rounded-[30px] p-1 flex items-center gap-[10px]">
-                        <div className=''>{deliveryMin} Min</div>
-                    </div>
+                    <div className=" bg-[#D63626] text-white rounded-[30px] p-1 flex items-center gap-[10px]">{deliveryMin} Min</div>
                 </div>
             </div>
         </div>
--- src/components/sections/Restaurants/RestaurantELements.tsx
+++ src/components/sections/Restaurants/RestaurantELements.tsx
@@ -11,6 +11,7 @@ type RestaurantsData = {
     description: string;
     deliveryPrice: number;
     deliveryMin: number;
+    new_item?: boolean;
 }
 
 
@@ -19,9 +20,10 @@ const Restaurantsdata: RestaurantsData[] = [
         id: 0,
         image: "https://picsum.photos/25/25",
         title: "Chinese",
-        description: "string",
+        description: "dddddddddddd dddddddddddddddddddddddddddddddddddddddddddddddddd",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: false
     },
     {
         id: 1,
@@ -29,7 +31,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: true
     },
     {
         id: 2,
@@ -37,7 +40,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: false
     },
     {
         id: 3,
@@ -45,7 +49,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: false
     },
     {
         id: 4,
@@ -53,7 +58,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: false
     },
     {
         id: 5,
@@ -61,7 +67,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: true
     },
     {
         id: 6,
@@ -69,7 +76,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: true
     },
     {
         id: 7,
@@ -77,7 +85,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: false
     },
     {
         id: 8,
@@ -85,7 +94,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: false
     },
     {
         id: 9,
@@ -93,7 +103,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: true
     },
     {
         id: 10,
@@ -101,7 +112,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: true
     },
     {
         id: 11,
@@ -109,7 +121,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: true
     },
     {
         id: 12,
@@ -117,7 +130,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: true
     },
     {
         id: 13,
@@ -125,7 +139,8 @@ const Restaurantsdata: RestaurantsData[] = [
         title: "Chinese",
         description: "string",
         deliveryPrice: 57,
-        deliveryMin: 5
+        deliveryMin: 5,
+        new_item: false
     }
 ];
 
