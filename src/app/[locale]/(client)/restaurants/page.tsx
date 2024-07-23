de4407569c7343037895963825e73fbb8891af73
Morqulis <morqqulis@gmail.com>
2024-07-23 11:59:49 +0400

    fixed restaurants routing

--- src/app/[locale]/(client)/restaurants/page.tsx
+++ src/app/[locale]/(client)/restaurants/page.tsx
@@ -1,29 +1,20 @@
-import { NextPage } from 'next';
-import { getTranslations } from 'next-intl/server';
-import RestaurantsHeader from '../../../../components/sections/Restaurants/RestaurantsHeader';
-import ProductList from '../../../../components/sections/Restaurants/ProductList';
-import Cart from '../../../../components/sections/Restaurants/Cart';
+import { NextPage } from "next";
+import { getTranslations } from "next-intl/server";
+import RestaurantsHeader from "../../../../components/sections/Restaurants/RestaurantsHeader";
+import ProductList from "../../../../components/sections/Restaurants/ProductList";
+import Cart from "../../../../components/sections/Restaurants/Cart";
 
 export async function generateMetadata({ params: { locale } }) {
-  const t = await getTranslations({ locale, namespace: 'Restaurants.Metadata' });
+  const t = await getTranslations({ locale, namespace: "Restaurants.Metadata" });
 
   return {
-    title: t('title'),
-    description: t('description'),
+    title: t("title"),
+    description: t("description"),
   };
 }
 
 const RestaurantsPage: NextPage = (): JSX.Element => {
-  return (
-    
-    <div className="min-h-screen">
-      <RestaurantsHeader />
-      <main className="container flex p-4">
-        <ProductList />
-        <Cart />
-      </main>
-    </div>
-  );
+  return <main>Pakize Restaurants !</main>;
 };
 
 export default RestaurantsPage;
