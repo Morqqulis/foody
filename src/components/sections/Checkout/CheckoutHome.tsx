4d21018aca33fd96db8caf18faba1bcb9a7df7ca
Morqulis <morqqulis@gmail.com>
2024-07-28 16:27:46 +0400

    Fixed Checkout form

--- src/components/sections/Checkout/CheckoutHome.tsx
+++ src/components/sections/Checkout/CheckoutHome.tsx
@@ -5,11 +5,49 @@ import CheckoutOrder from "./CheckoutOrder";
 import DoneIconComponent from "@sections/Checkout/LottieAnimation";
 import { getDocuments } from "../../../utls/functions";
 import { collections, databases, dbId, ID } from "@libs/appwrite/config";
+import { Button } from "@ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@ui/form";
+import { Input } from "@ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Label } from "@ui/label";
+import { RadioGroup, RadioGroupItem } from "@ui/radio-group";
 
-const phonePrefixes = ["050", "051", "055", "070", "010", "099", "077"];
+// const phonePrefixes = ["050", "051", "055", "070", "010", "099", "077"];
+
+const formSchema = z.object({
+  address: z
+    .string({
+      required_error: "Delivery address is required.",
+    })
+    .min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  phone: z
+    .string({
+      required_error: "Contact number is required.",
+      invalid_type_error: "Contact number must be a number.",
+    })
+    .min(2, {
+      message: "Username must be at least 11 characters.",
+    }),
+  payment: z.enum(["Cash", "Card"], {
+    required_error: "Payment method is required.",
+  }),
+});
 
 function CheckoutHome() {
-  const [prefix, setPrefix] = useState(phonePrefixes[0]);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      address: "",
+      phone: '',
+      payment: "Cash",
+    },
+  });
+
+  //   const [prefix, setPrefix] = useState(phonePrefixes[0]);
   const [phoneNumber, setPhoneNumber] = useState("");
   const [showDoneIcon, setShowDoneIcon] = useState(false);
 
@@ -31,7 +69,7 @@ function CheckoutHome() {
         setBasket(JSON.parse(prevBasket));
       }
     })();
-  }, []);
+  }, [userId]);
 
   const handleCheckOut = async (v: any) => {
     setShowDoneIcon(true);
@@ -55,20 +93,20 @@ function CheckoutHome() {
     setShowDoneIcon(false);
   };
 
-  const handlePrefixChange = (event: FormEvent<HTMLSelectElement>) => {
-    setPrefix(event.currentTarget.value);
-  };
+  //   const handlePrefixChange = (event: FormEvent<HTMLSelectElement>) => {
+  //     // setPrefix(event.currentTarget.value);
+  //   };
 
-  const handlePhoneNumberChange = (event: FormEvent<HTMLInputElement>) => {
-    const input = event.currentTarget;
-    input.value = input.value.replace(/[^0-9]/g, "");
-    setPhoneNumber(input.value);
-  };
+  //   const handlePhoneNumberChange = (event: FormEvent<HTMLInputElement>) => {
+  //     const input = event.currentTarget;
+  //     input.value = input.value.replace(/[^0-9]/g, "");
+  //     setPhoneNumber(input.value);
+  //   };
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    alert(`Phone Number: ${prefix}${phoneNumber}`);
-  };
+  //   const handleSubmit = (event: FormEvent) => {
+  //     event.preventDefault();
+  //     // alert(`Phone Number: ${prefix}${phoneNumber}`);
+  //   };
 
   // useEffect(() => {
   //   if (showDoneIcon) {
@@ -80,73 +118,120 @@ function CheckoutHome() {
   //   }
   // }, [showDoneIcon]);
 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return showDoneIcon ? (
     <DoneIconComponent />
   ) : (
-    <div className={" mb-[150px]  grid grid-flow-col  justify-stretch "}>
-      <div className={"h-[515px] w-[618px] rounded bg-gray-7 px-7 "}>
-        <h2 className={"my-[20px] text-3xl font-semibold  text-[rgba(79,79,79,1)]"}>Checkout</h2>
-        <form onSubmit={handleSubmit} action="" className={"flex flex-col  gap-y-[35px] "}>
-          <div>
-            <label htmlFor="" className={"font-semibold text-[rgba(79,79,79,1)]"}>
-              Delivery Address
-            </label>
-            <input className={"mt-[10px] h-[53px] w-[546px] rounded pl-5"} type="text" placeholder="Ataturk 45 Ganclik Baku" />
-          </div>
-          <div>
-            <div>
-              <label htmlFor="" className={"mb-[10px] font-semibold text-[rgba(79,79,79,1)]"}>
-                Contact Number
-              </label>
-              <div className="flex items-center space-x-2"></div>
-              <select className="mt-[10px] h-[53px] rounded pl-0" value={prefix} onChange={handlePrefixChange}>
-                {phonePrefixes.map((code) => (
-                  <option key={code} value={code}>
-                    {code}
-                  </option>
-                ))}
-              </select>
-              <input
-                className="mt-[10px] h-[53px] w-[490px] rounded pl-5"
-                type="tel"
-                required
-                id="phone"
-                name="phone"
-                placeholder="Enter phone number"
-                maxLength={9}
-                value={phoneNumber}
-                onInput={handlePhoneNumberChange}
+    <section className={`w-full`}>
+      <div className={`flex items-start gap-4`}>
+        <div className={`w-full basis-2/3 rounded-md bg-gray-7 p-10 pb-11`}>
+          <h1 className={`mb-6  text-3xl font-semibold text-gray-2 `}>Checkout</h1>
+          <Form {...form}>
+            <form className={`flex flex-col gap-8`} onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem className={`flex flex-col gap-1`}>
+                    <FormLabel className={`text-lg font-semibold text-gray-2`}>Delivery Address</FormLabel>
+                    <FormControl>
+                      <Input
+                        className={`p-6 text-lg placeholder:text-foreground placeholder:duration-300 focus-visible:placeholder:opacity-0`}
+                        placeholder="Ataturk 45 Ganclik Baku"
+                        {...field}
+                        type={"text"}
+                        autoComplete={"address-level1 webauthn"}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
               />
-            </div>
-          </div>
-          <div>
-            <label htmlFor="" className={"font-semibold text-[rgba(79,79,79,1)]"}>
-              Payment Method
-            </label>
-            <div className={"mt-[6px] flex flex-row gap-[79px]"}>
-              <div className="flex items-center gap-2">
-                <input type="radio" name="checkout" id="cashCheck" />
-                <label htmlFor="cashCheck">pay at the door</label>
-              </div>
-              <div className="flex items-center gap-2">
-                <input type="radio" name="checkout" id="cardCheck" />
-                <label htmlFor="cardCheck">pay at the door by credit card</label>
-              </div>
-            </div>
-          </div>
-          <button
-            onClick={() => handleCheckOut({ deliveryAddress: "baku", paymentMethod: "card", contact: "0555859885" })}
-            className={
-              "mt-[20px] h-[53px] w-[546px] rounded border-2 bg-lime-600 text-white duration-500 hover:border-lime-600  hover:bg-white hover:text-lime-600"
-            }
-          >
-            Checkout
-          </button>
-        </form>
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem className={`flex flex-col gap-1`}>
+                    <FormLabel className={`text-lg font-semibold text-gray-2`}>Contact Number</FormLabel>
+                    <FormControl>
+                      <Input
+                        className={`p-6 text-lg placeholder:text-foreground placeholder:duration-300 focus-visible:placeholder:opacity-0`}
+                        placeholder="+994"
+                        {...field}
+                        type={"tel"}
+                        autoComplete={"tel webauthn"}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="payment"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel></FormLabel>
+                    <FormControl>
+                      <RadioGroup className={`flex items-center gap-20`} onValueChange={field.onChange} defaultValue={field.value}>
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem
+                              className={` h-[30px] w-[30px]  first:fill-[#6FCF97] ${field.value === "Card" ? "border-[#6FCF97] text-[#6FCF97]" : "border-foreground text-foreground"}`}
+                              value="Card"
+                            />
+                          </FormControl>
+                          <FormLabel
+                            className={`${field.value === "Card" ? "font-bold text-[#6FCF97]" : "text-foreground"} cursor-pointer text-base`}
+                          >
+                            pay at the door
+                          </FormLabel>
+                        </FormItem>
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem
+                              className={`h-[30px] w-[30px] first:fill-[#6FCF97] ${field.value === "Cash" ? "!border-[#6FCF97] text-[#6FCF97]" : "border-foreground text-foreground"}`}
+                              value="Cash"
+                            />
+                          </FormControl>
+                          <FormLabel
+                            className={`${field.value === "Cash" ? "font-bold text-[#6FCF97]" : "border-slate-600"} text-base} cursor-pointer`}
+                          >
+                            pay at the door by credit card
+                          </FormLabel>
+                        </FormItem>
+                      </RadioGroup>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button className={`bg-[#6FCF97] py-7`} type="submit">
+                Submit
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <CheckoutOrder basket={basket} />
       </div>
-      <CheckoutOrder basket={basket} />
-    </div>
+    </section>
   );
 }
 
 export default CheckoutHome;
+
+{
+  /* <CheckoutOrder basket={basket} /> */
+}
+// <button
+//   onClick={() => handleCheckOut({ deliveryAddress: "baku", paymentMethod: "card", contact: "0555859885" })}
+//   className={
+//     "mt-[20px] h-[53px] w-[546px] rounded border-2 bg-lime-600 text-white duration-500 hover:border-lime-600  hover:bg-white hover:text-lime-600"
+//   }
+// >
+//   Checkout
+// </button>;
--- src/components/sections/Checkout/CheckoutOrder.tsx
+++ src/components/sections/Checkout/CheckoutOrder.tsx
@@ -5,33 +5,26 @@ interface IProps {
 }
 
 const CheckoutOrder: React.FC<IProps> = ({ basket }): JSX.Element => {
-
-
   function getTotal() {
     return basket.reduce((total: number, data: any) => total + Number(data.price) * Number(data.quantity), 0);
   }
-  
-
+  console.log(basket);
   return (
-    <div className={"flex h-[500px] w-[397px] flex-col items-center justify-between rounded bg-gray-7"}>
-      <h2 className={"mt-[20px] flex w-[324px] items-center  justify-center text-lg font-medium"}>Your Order</h2>
-
-      <ul className={"flex h-[300px] max-h-[80%] w-[324px] flex-col  overflow-auto"}>
-        {basket.map((order: any) => (
-          <li className={"flex justify-between"} key={order.$id}>
-            <div className="flex gap-[10px]">
-              <span className={"text-lg font-medium"}>{order.quantity}</span>
-              <p>* {order.name}</p>
+    <div className={`h-full min-h-[370px] w-full max-w-[400px] bg-gray-7 px-10 py-5`}>
+      <h2 className={`text-center text-lg font-medium`}>Your Order</h2>
+      {basket.length > 0 &&
+        basket.map((item) => (
+          <div key={item.$id}>
+            <div>
+              <div className={`flex items-center gap-1 text-sm font-normal`}>
+                <span className={`text-lg font-medium`}>{item.quantity} x</span>
+                <span className={``}>{item.name}</span>
+              </div>
+              <span>{item.price}</span>
             </div>
-            <span>${Number(order.price).toFixed(2)}</span>
-          </li>
+            <span>{getTotal().toFixed(2)}</span>
+          </div>
         ))}
-      </ul>
-      <div className={"flex h-[13%] w-[324px] items-center justify-center border-t"}>
-        <div className={"flex w-full justify-between"}>
-          <p>Total</p>${getTotal().toFixed(2)}
-        </div>
-      </div>
     </div>
   );
 };
