cb26bb35b4f6fec1e14ed8fce3a9d5989c928e7a
Fidan-Alizada <160149958+Fidan-Alizada@users.noreply.github.com>
2024-07-29 17:53:42 +0400

    translation "client restaurants"

--- src/components/sections/Restaurants/RestaurantSection.tsx
+++ src/components/sections/Restaurants/RestaurantSection.tsx
@@ -6,6 +6,7 @@ import { FC, useEffect, useState } from "react";
 import { Vortex } from "react-loader-spinner";
 import { getListDocuments } from "../../../utls/functions";
 import RestaurantCard from "./RestaurantCard";
+import { useTranslations } from "next-intl";
 
 interface IRestaurant {
   name: string;
@@ -27,6 +28,7 @@ interface ICategory {
 }
 
 export const RestaurantSection: FC = (): JSX.Element => {
+  const t = useTranslations("RestaurantSection");
   const [restaurants, setRestaurants] = useState([]);
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -47,8 +49,6 @@ export const RestaurantSection: FC = (): JSX.Element => {
     })();
   }, [selectedCategory]);
 
-
-  
   const handleGetAllRestaurants = async () => {
     try {
       const { documents } = await getListDocuments(collections.restaurantsId);
@@ -69,20 +69,20 @@ export const RestaurantSection: FC = (): JSX.Element => {
     <div className="container flex items-start gap-10 py-5">
       <div className="flex h-[calc(90vh-130px)] w-full basis-1/5 flex-col gap-8 overflow-y-auto bg-gray-7 px-5 py-10">
         <Button
-          className={`flex w-full items-center  gap-4  text-xl font-medium text-mainBlack hover:bg-red-400 ${selectedCategory === null ? "bg-mainRed text-white" : ""}`}
+          className={`flex w-full items-center gap-4 text-xl font-medium text-mainBlack hover:bg-red-400 ${selectedCategory === null ? "bg-mainRed text-white" : ""}`}
           type={"button"}
-          aria-label={"food category"}
+          aria-label={t("ariaLabelFoodCategory")}
           variant={"ghost"}
           onClick={handleGetAllRestaurants}
         >
-          All
+          {t("all")}
         </Button>
 
         {categories?.map((category) => (
           <Button
-            className={`flex w-full items-center justify-start gap-4 text-left text-xl font-medium text-mainBlack hover:bg-red-400  ${selectedCategory === category.$id ? "bg-mainRed text-white" : ""}`}
+            className={`flex w-full items-center justify-start gap-4 text-left text-xl font-medium text-mainBlack hover:bg-red-400 ${selectedCategory === category.$id ? "bg-mainRed text-white" : ""}`}
             type={"button"}
-            aria-label={"food category"}
+            aria-label={t("ariaLabelFoodCategory")}
             key={category.$id}
             variant={"ghost"}
             onClick={() => handleCategoryClick(category.$id)}
@@ -92,15 +92,13 @@ export const RestaurantSection: FC = (): JSX.Element => {
           </Button>
         ))}
       </div>
-      <div
-        className={`grid h-full w-full grow basis-4/5 grid-cols-1 gap-5 overflow-y-auto p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-10`}
-      >
+      <div className={`grid h-full w-full grow basis-4/5 grid-cols-1 gap-5 overflow-y-auto p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-10`}>
         {restaurants?.length === 0 && (
           <Vortex
             wrapperClass={`col-span-4 w-full row-span-4 flex flex-col items-center justify-center`}
             height={500}
             width={500}
-            ariaLabel="vortex-loading"
+            ariaLabel={t("ariaLabelVortexLoading")}
             wrapperStyle={{}}
             colors={["#D63626", "green", "blue", "yellow", "orange", "purple"]}
           />
