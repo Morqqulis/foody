0d5a32f3a25091b8e1d6fc0512d8c7b32b7f5305
Fidan-Alizada <160149958+Fidan-Alizada@users.noreply.github.com>
2024-07-29 18:44:19 +0400

    translation "user profile"

--- src/components/sections/Profile/ProfileSection.tsx
+++ src/components/sections/Profile/ProfileSection.tsx
@@ -1,12 +1,16 @@
-import ProfileForm from "./ProfileForm"
+import React from "react";
+import { useTranslations } from "next-intl";
+import ProfileForm from "./ProfileForm";
 
 interface IProfileSection {}
 
 const ProfileSection: React.FC = (): JSX.Element => {
+  const t = useTranslations("ProfileSection");
+
   return (
     <section className={`bg-gray-7 px-10 pb-20 pt-10`}>
       <div className="container">
-        <h1 className="mb-7 text-2xl font-bold text-gray-2">Profile</h1>
+        <h1 className="mb-7 text-2xl font-bold text-gray-2">{t("profile")}</h1>
         <ProfileForm />
       </div>
     </section>
