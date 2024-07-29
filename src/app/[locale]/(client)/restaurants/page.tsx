64ed252c4870f6966d297bc616992083c6d9e010
Morqulis <morqqulis@gmail.com>
2024-07-29 15:48:23 +0400

    check again

--- src/app/[locale]/(client)/restaurants/page.tsx
+++ src/app/[locale]/(client)/restaurants/page.tsx
@@ -1,6 +1,6 @@
-import { RestaurantSection } from '@sections/Restaurants/RestaurantSection'
-import { NextPage } from 'next'
-import { getTranslations } from 'next-intl/server'
+import RestaurantSection from "@sections/Restaurants/RestaurantSection";
+import { NextPage } from "next";
+import { getTranslations } from "next-intl/server";
 
 export async function generateMetadata({ params: { locale } }) {
   const t = await getTranslations({ locale, namespace: "Restaurants.Metadata" });
@@ -15,10 +15,10 @@ interface IRestaurants {}
 
 const RestaurantsPage: NextPage = (): JSX.Element => {
   return (
-    <main className='wrapper'>
-      <RestaurantSection/>
+    <main className="wrapper">
+      <RestaurantSection />
     </main>
-  )
-}
+  );
+};
 
 export default RestaurantsPage;
