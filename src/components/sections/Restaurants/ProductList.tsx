f3f2e16c18a632faf9c6c3b35af999f7c247e76e
aylaya <160137722+aylaya@users.noreply.github.com>
2024-07-31 03:17:51 +0400

    animation client product list

--- src/components/sections/Restaurants/ProductList.tsx
+++ src/components/sections/Restaurants/ProductList.tsx
@@ -1,54 +1,55 @@
-'use client'
-import { collections } from '@libs/appwrite/config'
-import { Plus } from 'lucide-react'
-import { useTranslations } from 'next-intl'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import styles from './Scroll.module.css'
-import { getDocuments } from '../../../utls/functions'
+'use client';
+import { collections } from '@libs/appwrite/config';
+import { Plus } from 'lucide-react';
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import styles from './Scroll.module.css';
+import { getDocuments } from '../../../utls/functions';
 
 interface CartItem {
-  $collectionId: string
-  $createdAt: string
-  $updatedAt: string
-  $permissions: string[]
-  name: string
-  price: string | number
-  quantity: number
-  image: string
-  imageId: string
-  $databaseId: string
-  $id: string
-  $tenant: string
-  description: string
+  $collectionId: string;
+  $createdAt: string;
+  $updatedAt: string;
+  $permissions: string[];
+  name: string;
+  price: string | number;
+  quantity: number;
+  image: string;
+  imageId: string;
+  $databaseId: string;
+  $id: string;
+  $tenant: string;
+  description: string;
 }
 
 interface IProductList {
-  restId: string
-  setBasket: React.Dispatch<React.SetStateAction<CartItem[]>>
+  restId: string;
+  setBasket: React.Dispatch<React.SetStateAction<CartItem[]>>;
 }
+
 const ProductList: React.FC<IProductList> = ({ restId, setBasket }) => {
-  const t = useTranslations('ProductList')
-  const [products, setProducts] = useState([])
+  const t = useTranslations('ProductList');
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    ;(async () => {
+    (async () => {
       try {
-        const { products } = await getDocuments(collections.restaurantsId, restId)
-        setProducts(products)
+        const { products } = await getDocuments(collections.restaurantsId, restId);
+        setProducts(products);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    })()
-  }, [restId])
+    })();
+  }, [restId]);
 
   const addToBasket = (product: CartItem) => {
     setBasket((prev: any) =>
       prev?.some((item: any) => item.$id === product.$id)
         ? prev.map((item: any) => (item.$id === product.$id ? { ...item, quantity: item.quantity + 1 } : item))
         : [...prev, { ...product, quantity: 1 }]
-    )
-  }
+    );
+  };
 
   return (
     <div className="basis-3.5/6 w-full">
@@ -57,9 +58,18 @@ const ProductList: React.FC<IProductList> = ({ restId, setBasket }) => {
         <div className={`${styles.customScrollbar} h-[60vh] space-y-4 overflow-auto`}>
           {products.length > 0 &&
             products.map((product: any) => (
-              <div key={product.$id} className="flex items-center justify-between rounded border-y-2 border-[#e0e0e0] bg-white p-4 shadow">
+              <div
+                key={product.$id}
+                className="flex items-center justify-between rounded border-y-2 border-[#e0e0e0] bg-white p-4 shadow transition-transform duration-300 hover:scale-105"
+              >
                 <div className="flex items-center">
-                  <Image className="h-20 w-20 rounded-lg" src={product.image} alt={product.name} width={80} height={80} />
+                  <Image
+                    className="h-20 w-20 rounded-lg"
+                    src={product.image}
+                    alt={product.name}
+                    width={80}
+                    height={80}
+                  />
                   <div className="ml-4">
                     <h3 className="text-md font-semibold">{product.name}</h3>
                     <p className="text-sm text-gray-500">{product.description}</p>
@@ -68,7 +78,7 @@ const ProductList: React.FC<IProductList> = ({ restId, setBasket }) => {
                 <div className="flex items-center">
                   <span className="text-lg font-semibold">${product.price}</span>
                   <button
-                    className="group/plus ml-4 flex h-10 w-10 items-center justify-center rounded-full border-2 border-[#BDBDBD] text-green-500 hover:border-0 hover:bg-green-500"
+                    className="group/plus ml-4 flex h-10 w-10 items-center justify-center rounded-full border-2 border-[#BDBDBD] text-green-500 transition-all duration-300 hover:border-0 hover:bg-green-500"
                     onClick={() => addToBasket(product)}
                   >
                     <Plus className={`text-[#BDBDBD] duration-300 group-hover/plus:text-white`} />
@@ -79,7 +89,7 @@ const ProductList: React.FC<IProductList> = ({ restId, setBasket }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
