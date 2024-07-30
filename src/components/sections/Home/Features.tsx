f230def3f982517d8dbeed4b169e012f143b2f0c
Fidan-Alizada <160149958+Fidan-Alizada@users.noreply.github.com>
2024-07-31 00:40:16 +0400

    animations in features part of home page

--- src/components/sections/Home/Features.tsx
+++ src/components/sections/Home/Features.tsx
@@ -32,16 +32,16 @@ const Features: React.FC<IFeatures> = (): JSX.Element => {
   const t = useTranslations('Home.Features')
 
   return (
-    <section className={`py-20`}>
+    <section className="py-20">
       <div className="container">
-        <div className={`text-center`}>
-          <Title className={`text-[40px] font-black leading-[70px]`} tag={'h2'} text={t('title')} />
-          <p className={`mx-auto mb-14 max-w-[780px] text-[22px] leading-[30px]`}>{t('text')}</p>
+        <div className="text-center">
+          <Title className="text-[40px] font-black leading-[70px]" tag={'h2'} text={t('title')} />
+          <p className="mx-auto mb-14 max-w-[780px] text-[22px] leading-[30px]">{t('text')}</p>
         </div>
         <div className="grid grid-cols-1 place-items-center items-start gap-14 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
           {FeaturesCardsData.map((item) => (
             <FeaturesCard
-              className={`h-full md:w-full md:max-w-full md:last:col-span-2 lg:max-w-[327px] lg:last:col-span-1 lg:last:w-auto lg:last:max-w-[327px]`}
+              className="h-full w-full max-w-full md:max-w-full lg:max-w-[327px] lg:last:col-span-1 lg:last:w-auto lg:last:max-w-[327px] transition-transform duration-500 ease-in-out hover:scale-105 animate-flip-down"
               key={item.id}
               title={t(`cardsTitle.${item.id}`)}
               text={t(`cardsText.${item.id}`)}
