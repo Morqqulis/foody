30dac0b2937519dc422fd3f67bc544ee51bff6a9
Morqulis <morqqulis@gmail.com>
2024-08-10 14:37:32 +0400

    some fixes

--- src/components/TopLoadingBar.tsx
+++ src/components/TopLoadingBar.tsx
@@ -1,36 +1,36 @@
-'use client';
+'use client'
 
-import React, { useState, useEffect } from 'react';
-import LoadingBar from 'react-top-loading-bar';
-import { usePathname, useSearchParams } from 'next/navigation';
+import React, { useState, useEffect } from 'react'
+import LoadingBar from 'react-top-loading-bar'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 const TopLoadingBar: React.FC = () => {
-  const [progress, setProgress] = useState(0);
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
+  const [progress, setProgress] = useState(0)
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
 
   useEffect(() => {
     const handleStart = () => {
-      setProgress(40);
-    };
+      setProgress(40)
+    }
 
     const handleComplete = () => {
-      setProgress(100);
-    };
+      setProgress(100)
+    }
 
-    handleStart();
+    handleStart()
 
     const timer = setTimeout(() => {
-      handleComplete();
-    }, 500);
+      handleComplete()
+    }, 500)
 
     return () => {
-      clearTimeout(timer);
-      setProgress(0);
-    };
-  }, [pathname, searchParams]);
+      clearTimeout(timer)
+      setProgress(0)
+    }
+  }, [pathname, searchParams])
 
-  return <LoadingBar color="#f11946" progress={progress} onLoaderFinished={() => setProgress(0)} />;
-};
+  return <LoadingBar color="#f11946" progress={progress} onLoaderFinished={() => setProgress(0)} />
+}
 
-export default TopLoadingBar;
+export default TopLoadingBar
